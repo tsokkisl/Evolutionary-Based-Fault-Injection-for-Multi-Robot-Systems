2cde0673568c8af12002242b2f27349cb535a478
tsokkisl <tsokkis.l@gmail.com>
2021-06-18 10:02:24 +0100

    FaultEngine generated class

---
+++ middleware/core/FaultEngine.java
@@ -0,0 +1,19 @@
+package dsl.dslloader;
+
+import dsl.*;
+import dsl.faults.*;
+import java.util.Optional;
+import java.util.List;
+import java.util.ArrayList;
+
+public class FaultEngine {
+	
+	public FaultEngine(Mission m) {
+		
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+	
+}
